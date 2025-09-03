268991eaa2144b7ed6ba5108aab78ee70fdf8de1
ShahdRaafat <shahdraafat.725@gmail.com>
2025-09-03 17:30:03 +0300

    refactor: restructure layout and add Header component

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -2,20 +2,34 @@ import Link from "next/link";
 
 function Navigation() {
   return (
-    <ul className=" p-8 text-4xl font-bold flex gap-5">
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/cabins">Cabins</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-      <li>
-        <Link href="/account">Account</Link>
-      </li>
-    </ul>
+    <nav className="text-xl z-10">
+      <ul className=" font-bold flex gap-16 items-center">
+        <li>
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,13 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/_styles/globals.css";
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
   title: {
     template: "%s / The Wild Oasis",
@@ -13,12 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className} flex flex-col antialiased`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
