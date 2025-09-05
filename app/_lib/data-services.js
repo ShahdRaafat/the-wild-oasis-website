0a982734c29fcac1b9b69b19969b030e48302807
ShahdRaafat <shahdraafat.725@gmail.com>
2025-09-05 23:41:31 +0300

    feat: add NotFound and Error components for improved error handling

--- app/_lib/data-services.js
+++ app/_lib/data-services.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import supabase from "./supabase";
+import { notFound } from "next/navigation";
 
 //Get
 export async function getCabin(id) {
@@ -11,6 +12,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
