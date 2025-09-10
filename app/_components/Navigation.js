95cc21b55d897d10c7aa1c0212ed0c77dd686dda
ShahdRaafat <shahdraafat.725@gmail.com>
2025-09-11 01:29:07 +0300

    refactor: improve Header and Navigation component architecture

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,45 +1,8 @@
-"use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { auth } from "@/app/_lib/auth";
+import NavigationClient from "./NavigationClient";
 
-function Navigation() {
-  const pathname = usePathname();
-  return (
-    <nav className="text-xl z-10">
-      <ul className=" font-bold flex gap-16 items-center">
-        <li>
-          <Link
-            href="/cabins"
-            className={`hover:text-accent-400 transition-colors ${
-              pathname.includes("/cabins") ? "text-accent-400" : ""
-            }`}
-          >
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/about"
-            className={`hover:text-accent-400 transition-colors ${
-              pathname.includes("/about") ? "text-accent-400" : ""
-            }`}
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/account"
-            className={`hover:text-accent-400 transition-colors ${
-              pathname.includes("/account") ? "text-accent-400" : ""
-            }`}
-          >
-            Guest area
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+async function Navigation() {
+  const session = await auth();
+  return <NavigationClient session={session} />;
 }
-
 export default Navigation;
---
+++ app/_components/NavigationClient.js
@@ -0,0 +1,66 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { auth } from "@/app/_lib/auth";
+import Image from "next/image";
+function Navigation({ session }) {
+  const pathname = usePathname();
+  return (
+    <nav className="text-xl z-10">
+      <ul className=" font-bold flex gap-16 items-center">
+        <li>
+          <Link
+            href="/cabins"
+            className={`hover:text-accent-400 transition-colors ${
+              pathname.includes("/cabins") ? "text-accent-400" : ""
+            }`}
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className={`hover:text-accent-400 transition-colors ${
+              pathname.includes("/about") ? "text-accent-400" : ""
+            }`}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          {session?.user.image ? (
+            <Link
+              href="/account"
+              className={`hover:text-accent-400 transition-colors flex items-center gap-4 ${
+                pathname.includes("/account") ? "text-accent-400" : ""
+              }`}
+            >
+              <Image
+                className=" rounded-full object-cover"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+                width={48}
+                height={48}
+              />
+
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className={`hover:text-accent-400 transition-colors ${
+                pathname.includes("/account") ? "text-accent-400" : ""
+              }`}
+            >
+              Guest area
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
