b93e16e243917b8c54228871a66f3ed50f3ca989
ShahdRaafat <shahdraafat.725@gmail.com>
2025-09-11 23:13:24 +0300

    feat: Implement sign-in and sign-out buttons with Google authentication

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
-function Page() {
+async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <h2 className="font-semibold text-accent-400 text-2xl mb-7">
-      Welcome , Shahoud
+      Welcome, {firstName}
     </h2>
   );
 }
--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,12 @@
-export const metadata = {
-  title: "Login",
-};
-function Page() {
-  return <div>Login Page</div>;
-}
+import SignInButton from "../_components/SignInButton";
 
-export default Page;
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-10 mt-10 items-center">
+      <h2 className="text-3xl font-semibold">
+        Sign in to access your guest area
+      </h2>
+      <SignInButton />
+    </div>
+  );
+}
